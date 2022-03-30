0477891d4e3245e960f97c3f20d67c658a478cec
repin <sasharepin@Mail.ru>
2022-03-30 07:58:06 +0300

    finish lesson 1-2

---
+++ ConsoleApp3/Lesson1/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWorkOOP.Lesson1
+{
+    public class Account
+    {
+        public static int countAccount;
+        private  int _nAccount;
+        private decimal _balance;
+        private typeAccount _typeAcc;
+
+        public Account()
+        {
+                _nAccount = ++countAccount;
+        }
+        public int NAccount 
+        {
+            get
+            {
+                return _nAccount;
+            }
+        }
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+
+        public typeAccount TypeAcc
+        {
+            get
+            {
+                return _typeAcc;
+            }
+            set
+            {
+                _typeAcc = value;
+            }
+        }
+    }
+
+    public enum typeAccount
+    {
+        Deposit = 1,
+        Credit = 2
+    }
+
+}
