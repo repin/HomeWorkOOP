75d6edabab0c208c310eff79ff86f323b902a594
repin <sasharepin@Mail.ru>
2022-04-03 18:03:01 +0300

    update lesson 2-2

--- ConsoleApp3/lesson2/lesson2-2.cs
+++ ConsoleApp3/lesson2/lesson2-2.cs
@@ -14,11 +14,11 @@ namespace HomeWorkOOP.lesson2
         {
             string stroka = "Какой чудесный день!";
             Console.WriteLine($"Исходная строка: {stroka}");
-            stroka = ReverseString(stroka);
+            ReverseString(ref stroka);
             Console.WriteLine($"Перевернутая строка: {stroka}");
         }
-
-        private string ReverseString(string stroka)
+/*
+        private string ReverseString(ref string stroka)
         {
             StringBuilder str = new StringBuilder();
             for (int i = stroka.Length - 1; i >= 0; i--)
@@ -26,6 +26,15 @@ namespace HomeWorkOOP.lesson2
                 str.Append(stroka[i]);
             }
             return str.ToString();
+        }*/
+        private void ReverseString(ref string stroka)
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = stroka.Length - 1; i >= 0; i--)
+            {
+                str.Append(stroka[i]);
+            }
+            stroka = str.ToString();
         }
     }
 }
